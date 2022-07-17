74583b6ae79314ae1ea35fb787821a52ef63d62e
Roberto Marçal <roberto.marcal@olxbr.com>
2022-07-17 08:28:14 -0300

    first commit

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+    default = "${AWS_ACCESS_KEY}"
+}
+
+variable "aws_secret_key" {
+    default = "${AWS_SECRET_KEY}"
+}
+
+variable "region" {
+    default = "${AWS_REGION}"
+} 
