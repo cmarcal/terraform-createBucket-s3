d54ad803f21d7fd8d754ef10e9b19d714644c6ec
Roberto Marçal <roberto.marcal@olxbr.com>
2022-07-17 10:44:11 -0300

    fix: set empty variables

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
-    default = "${AWS_ACCESS_KEY}"
+    default = ""
 }
 
 variable "aws_secret_key" {
-    default = "${AWS_SECRET_KEY}"
+    default = ""
 }
 
 variable "region" {
