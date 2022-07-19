d6a8303bc9c5c55c735c4639a4a60db7d30544c9
Roberto Marçal <roberto.marcal@olxbr.com>
2022-07-19 10:19:59 -0300

    feat: use new resouce to set acl

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -1,4 +1,9 @@
 resource "aws_s3_bucket" "bucket_pr" {
     bucket = "${var.bucket_name}" 
-    acl    = "${var.acl_value}"   
+}
+
+resource "aws_s3_bucket_acl" "bucket_pr_acl" {
+    bucket = aws_s3_bucket.bucket_pr.id
+    acl    = "${var.acl_value}"
+
 }
