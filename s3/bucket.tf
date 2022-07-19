84bab8e0bf231a18ee40541ea52beefdb8ddd045
Roberto Marçal <roberto.marcal@olxbr.com>
2022-07-19 13:51:30 -0300

    try use source

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -1,9 +1,11 @@
 resource "aws_s3_bucket" "bucket_pr" {
-    bucket = "${var.bucket_name}" 
+    source        = "terraform-aws-modules/s3-bucket/aws"
+    create_bucket = false
+
+    bucket        = "${var.bucket_name}" 
 }
 
 resource "aws_s3_bucket_acl" "bucket_pr_acl" {
     bucket = aws_s3_bucket.bucket_pr.id
     acl    = "${var.acl_value}"
-
 }
