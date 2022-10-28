bae85f7f777c0a108c84388d865a5c6589f26452
cmarcal <cmarcal.roberto@gmail.com>
2022-10-27 23:24:48 -0300

    fix: set destroy bucket in correctly place

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucket_pr" {
-    bucket = "${var.bucket_name}" 
+    bucket        = "${var.bucket_name}" 
+    force_destroy = true
 }
 
 resource "aws_s3_bucket_acl" "bucket_pr_acl" {
-    bucket        = aws_s3_bucket.bucket_pr.id
-    force_destroy = true
-    acl           = "${var.acl_value}"
+    bucket = aws_s3_bucket.bucket_pr.id
+    acl    = "${var.acl_value}"
 }
